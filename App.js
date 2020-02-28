c8927a120a40a3266d7851dfe63914e64f87d430
DillonMcQ <mcquerry.dillon@gmail.com>
2020-02-28 10:20:25 -0600

    Basic setup, button and wku logo

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import logo from './logo/wku.jpg'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      <View style={styles.container}>
+        <Image source = {logo} style = {styles.logo} />
+
+        <TouchableOpacity
+            onPress = {() => alert('Information Here')}
+            style = {styles.button}>
+          <Text style = {styles.buttonText}> Check Diddle North </Text>
+        </TouchableOpacity>
+      </View>
   );
 }
 
@@ -15,5 +22,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 300,
+    height: 300,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
   },
 });
